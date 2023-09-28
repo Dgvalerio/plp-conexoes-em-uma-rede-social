748137d9877e7f5066d80a6627dc4bb029c48205
davigvalerio <al.davigvalerio@gmail.com>
2023-09-28 19:11:09 -0300

    docs: add consult examples

--- connections.pl
+++ connections.pl
@@ -19,11 +19,21 @@ follow(darlan, silvana).
 level1(Follower, Followed) :-
     follow(Follower, Followed).
 
+/*
+Consulte com:
+? - findall(Followed, level1(joao, Followed), Follows).
+*/
+
 % Contatos de nível 2 – aqueles que possuem conhecimento indireto, com um nível de indireção: amigos de amigos.
 level2(Follower, Followed) :-
     follow(Follower, Intermediary),
     follow(Intermediary, Followed),
     not(level1(Follower, Followed)).
+
+/*
+Consulte com:
+? - findall(Followed, level2(joao, Followed), Follows).
+*/
 
 % Contatos de nível 3 – aqueles que possuem conhecimento indireto, com dois níveis de indireção: amigos de amigos de amigos.
 level3(Follower, Followed) :-
@@ -31,6 +41,11 @@ level3(Follower, Followed) :-
     level2(Intermediary, Followed),
     not(level1(Follower, Followed)),
     not(level2(Follower, Followed)).
+
+/*
+Consulte com:
+? - findall(Followed, level3(joao, Followed), Follows).
+*/
 
 % Contatos distantes – aqueles que possuem conhecimento indireto de amizade, com mais de dois níveis de indireção.
 allLevels(Follower, Followed):-
@@ -45,3 +60,8 @@ levelGreaterThan3(Follower, Followed) :-
     not(level1(Follower, Followed)),
     not(level2(Follower, Followed)),
     not(level3(Follower, Followed)).
+
+/*
+Consulte com:
+? - findall(Followed, levelGreaterThan3(joao, Followed), Follows).
+*/
